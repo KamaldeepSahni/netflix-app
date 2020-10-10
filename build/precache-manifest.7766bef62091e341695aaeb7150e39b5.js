9d0683107442ce34273ec18b47b346e06e6adfe9
Kamaldeep <ksahni687@gmail.com>
2020-10-10 10:55:44 +0530

    Added build dir

---
+++ build/precache-manifest.7766bef62091e341695aaeb7150e39b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc419e1ce4f348a7b5c183ac64168f4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26af0a0206a0efbd489a",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "26af0a0206a0efbd489a",
+    "url": "/static/js/2.a4c22d6a.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.a4c22d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d08898d9d99e90be35",
+    "url": "/static/js/main.f03c0ebc.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/static/media/logo.6dbba458.svg"
+  }
+]);
